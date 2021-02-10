c1c13c99a7a81da7772deb51782f8a9a55fa9adc
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-02-09 22:40:59 -0800

    Added server to include models and routes folders.

---
+++ server.js
@@ -0,0 +1,35 @@
+// Requiring necessary npm packages
+const express = require( "express" );
+const logger = require( "morgan" );
+const mongoose = require( "mongoose" );
+
+// Requiring the models folder before connecting to the server
+require("./models");
+
+// Setting up PORT for Heroku connection
+const PORT = process.env.PORT || 3000;
+
+// Creating express app
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Connecting to MongoDB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", {useNewUrlParser: true});
+
+// Requiring Routes
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
+
+// Start Server
+app.listen(PORT, () => {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+});
