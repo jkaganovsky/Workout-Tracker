f87220c836ab123e574cb4502f6683655a4b4121
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-02-09 22:49:33 -0800

    Added api get, post, and put routes for workouts to render on the page.

---
+++ routes/apiRoutes.js
@@ -0,0 +1,43 @@
+// Requiring express package
+const router = require( "express" ).Router();
+
+// Requiring workout.js from models folder
+const { Workout } = require( "../models" );
+
+// Getting all workouts to render on the page
+router.get( "/api/workouts", ( req, res)  => {
+
+    Workout.find()
+           .then(( workouts ) => {
+        res.json( workouts );
+    }).catch(( err ) => {
+        res.status( 500 ).json( err );
+    });
+
+});
+
+// Posting the newly created workout on the page
+router.post( "/api/workouts", ( req, res ) => {
+
+    Workout.create( req.body )
+           .then(( workout ) => {
+        res.json( workout );
+    }).catch(( err ) => {
+        res.status( 500 ).json( err );
+    });
+
+});
+
+// Viewing the newly created workout associated with an ID to the page
+router.put( "/api/workouts/:id", ( req, res ) => {
+
+    const newExercise = req.body;
+
+    Workout.findByIdAndUpdate( req.params.id, { $push: { exercises: newExercise }})
+});
+
+// router.post("/api/workouts/range", (req, res) => {
+
+// });
+
+module.exports = router;
