82ef93322521878e81b68f5f4727be7a65123bd9
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-02-09 22:50:22 -0800

    Added html routes to get the main, exercise, and stat html to render on the page.

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,20 @@
+// Requiring path in order to use relative routes to the HTML files
+const router = require("express").Router();
+const path = require("path");
+
+// Main Page
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+// Exercise Page
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+// Stats Page
+router.get("/stats", (req,res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router;
