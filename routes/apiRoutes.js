95264286f0c3645a5efb2c3280a32f061b2c36d3
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-02-10 23:27:18 -0800

    Added new: true to router.put on api/workouts/:id.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -33,11 +33,24 @@ router.put( "/api/workouts/:id", ( req, res ) => {
 
     const newExercise = req.body;
 
-    Workout.findByIdAndUpdate( req.params.id, { $push: { exercises: newExercise }})
+    console.log( newExercise );
+
+    Workout.findByIdAndUpdate( req.params.id, {
+        $push: {
+            exercises: newExercise
+        }
+    }, {
+        new: true
+    }).then(( workout ) => {
+        res.json( workout );
+    }).catch((err) => {
+        console.log(err);
+        res.status( 500 ).json( err );
+    });
 });
 
-// router.post("/api/workouts/range", (req, res) => {
-
-// });
+router.get("/api/workouts/range", (req, res) => {
+    
+});
 
 module.exports = router;
